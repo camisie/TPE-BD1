b1993144d84fe166a885cdc5292b67f5d8761b6e
cami <csierra@itba.edu.ar>
2023-06-12 14:51:18 -0300

    Function ReporteConsolidado working

--- funciones.sql
+++ funciones.sql
@@ -4,6 +4,8 @@ drop table IF EXISTS ANIO cascade ;
 drop table IF EXISTS NIVEL_EDUCACION cascade ;
 drop table IF EXISTS BIRTHS;
 drop trigger IF EXISTS insertarDatosTablaDefinitiva ON BIRTHS;
+-- drop function IF EXISTS ReporteConsolidado(INT);
+-- drop function IF EXISTS reporteconsolidado(cantidad_de_anios integer);
 
 -- Tablas de las dimensiones
 
@@ -47,15 +49,15 @@ CREATE TABLE BIRTHS (
 );
 
 
---funcion para saber si es año biciesto
+--Funcion para saber si es año bisiesto
 
-CREATE OR REPLACE FUNCTION esBiciesto(anio INT) RETURNS BOOLEAN AS $$
+CREATE OR REPLACE FUNCTION esBisiesto(anio INT) RETURNS BOOLEAN AS $$
 	BEGIN
 		RETURN anio % 4 = 0 AND (anio % 100 != 0 OR anio % 400 = 0);
 	END;
 $$ LANGUAGE plpgSQL;
 
---trigger
+--Trigger
 
 CREATE OR REPLACE FUNCTION insertarDatosTablaDefinitiva() RETURNS trigger AS $$
 	DECLARE
@@ -74,7 +76,7 @@ CREATE OR REPLACE FUNCTION insertarDatosTablaDefinitiva() RETURNS trigger AS $$
 
 		IF new.year NOT IN (SELECT anio FROM ANIO) THEN
 			id_anio = new.year;
-		    INSERT INTO ANIO VALUES (id_anio, esBiciesto(new.year));
+		    INSERT INTO ANIO VALUES (id_anio, esBisiesto(new.year));
 		ELSE
 			id_anio := (SELECT anio FROM ANIO WHERE anio = new.year);
 		END IF;
@@ -102,19 +104,152 @@ FOR EACH ROW
 EXECUTE PROCEDURE insertarDatosTablaDefinitiva();
 
 
---copiamos los datos del csv a la tabla definitiva
+--Copiamos los datos del csv a la tabla definitiva
 
 \COPY BIRTHS(state, state_abbreviation, year, gender, mother_education_level, education_level_code, births, mother_average_age, average_birth_weight) FROM 'us_births_2016_2021.csv' DELIMITER ',' CSV HEADER;
--- select * from BIRTHS;
--- SELECT * FROM estado;
--- SELECT * FROM anio;
--- SELECT * FROM nivel_educacion;
+select * from BIRTHS;
+SELECT * FROM estado;
+SELECT * FROM anio;
+SELECT * FROM nivel_educacion;
 
 
---funcion ReporteConsolidado(n)
+--Funcion ReporteConsolidado(n)
 
 CREATE OR REPLACE FUNCTION ReporteConsolidado(cantidad_de_anios INT) RETURNS VOID RETURNS NULL ON NULL INPUT AS $$
+    DECLARE
+		current_year anio.anio%TYPE;
+		max_year anio.anio%TYPE;
+		year TEXT;
+
+		Total INT;
+        AvgAge FLOAT;
+		MinAge FLOAT;
+		MaxAge FLOAT;
+		AvgWeight FLOAT;
+		MinWeight FLOAT;
+		MaxWeight FLOAT;
+
+		r_anio RECORD;
+		r_state RECORD;
+		r_gender RECORD;
+		r_education_level RECORD;
+
+	--cursor que devuelve resultados por Estado
+	c_State CURSOR (anioIn INT) FOR
+	       SELECT nombre, SUM(births) AS Total,  AVG(mother_average_age) AS AvgAge, MIN(mother_average_age) AS MinAge, MAX(mother_average_age) AS MaxAge, AVG(average_birth_weight) AS AvgWeight, MIN(average_birth_weight) AS MinWeight, MAX(average_birth_weight) AS MaxWeight
+	       FROM BIRTHS JOIN ESTADO ON BIRTHS.state_abbreviation = ESTADO.id
+-- 	       WHERE anioIn = BIRTHS.year
+	       GROUP BY nombre
+	       ORDER BY nombre DESC ;
+
+	--cursor que devuelve resultados por Genero
+	c_Gender CURSOR (anioIn INT) FOR
+	       SELECT gender, SUM(births) AS Total, AVG(mother_average_age) AS AvgAge, MIN(mother_average_age) AS MinAge, MAX(mother_average_age) AS MaxAge, AVG(average_birth_weight) AS AvgWeight, MIN(average_birth_weight) AS MinWeight, MAX(average_birth_weight) AS MaxWeight
+	       FROM BIRTHS
+	       WHERE anioIn = BIRTHS.year
+	       GROUP BY gender
+	       ORDER BY gender;
+
+	--cursor que devuelve resultados por Educacion
+	c_Education CURSOR (anioIn INT) FOR
+	   SELECT descripcion, SUM(births) AS Total, AVG(mother_average_age) AS AvgAge, MIN(mother_average_age) AS MinAge, MAX(mother_average_age) AS MaxAge, AVG(average_birth_weight) AS AvgWeight, MIN(average_birth_weight) AS MinWeight, MAX(average_birth_weight) AS MaxWeight
+	   FROM BIRTHS JOIN NIVEL_EDUCACION ON BIRTHS.education_level_code = NIVEL_EDUCACION.nivel
+	   WHERE anioIn = BIRTHS.year
+	   GROUP BY descripcion
+	   ORDER BY descripcion;
+
+	--cursor que devuelve resultados por Año
+	c_Anio CURSOR(anioIn INT) FOR
+	   SELECT anio, SUM(births) AS Total, AVG(mother_average_age) AS AvgAge, MIN(mother_average_age) AS MinAge, MAX(mother_average_age) AS MaxAge, AVG(average_birth_weight) AS AvgWeight, MIN(average_birth_weight) AS MinWeight, MAX(average_birth_weight) AS MaxWeight
+	   FROM BIRTHS JOIN anio ON BIRTHS.year = anio.anio
+	   WHERE anioIn = BIRTHS.year
+	   GROUP BY anio
+	   ORDER BY anio;
+
+
     BEGIN
-		-- todo
+		IF (cantidad_de_anios <= 0) THEN
+            RETURN;
+        END IF;
+
+        SELECT MIN(anio) FROM anio INTO current_year;
+
+		IF (current_year IS NULL) THEN
+			RAISE EXCEPTION 'No hay valores ingresados';
+		END IF;
+
+        max_year = current_year + cantidad_de_anios - 1;
+
+		IF (cantidad_de_anios > 0) THEN
+            IF max_year > (SELECT MAX(anio) FROM anio)
+	        THEN SELECT MAX(anio) FROM anio INTO max_year;
+	    END IF;
+
+    	RAISE NOTICE '---------------------------------------------';
+	    RAISE NOTICE '-------CONSOLIDATED BIRTH REPORT----------';
+	    RAISE NOTICE '---------------------------------------------';
+	    RAISE NOTICE 'Year---Category-------------------Total---AvgAge---MinAge---MaxAge---AvgWeight---MinWeight---MaxWeight';
+
+            WHILE (current_year <= max_year) LOOP
+                year := current_year;
+                total := 0;
+				avgAge := 0;
+				minAge := 0;
+				maxAge := 0;
+				avgWeight := 0;
+				minWeight := 0;
+				maxWeight := 0;
+
+
+	        	RAISE NOTICE '--------------------------------------------------------------------------';
+
+				OPEN c_State(anioIn := current_year);
+					LOOP
+						FETCH c_State INTO r_state;
+						EXIT WHEN NOT FOUND;
+						RAISE NOTICE '%   State: % - % - % - % - % - % - % - %', year, r_state.nombre::TEXT, r_state.total::INT, r_state.avgAge::FLOAT, r_state.minAge::FLOAT, r_state.maxAge::FLOAT, r_state.avgWeight::FLOAT, r_state.minWeight::FLOAT, r_state.maxWeight::FLOAT;
+						year := '----';
+					END LOOP;
+				CLOSE c_State;
+
+				OPEN c_Gender(anioIn := current_year);
+					LOOP
+						FETCH c_Gender INTO r_gender;
+						EXIT WHEN NOT FOUND;
+						RAISE NOTICE '%   Gender: Male - % - % - % - % - % - % - %', year, r_gender.total::INT, r_gender.avgAge::FLOAT, r_gender.minAge::FLOAT, r_gender.maxAge::FLOAT, r_gender.avgWeight::FLOAT, r_gender.minWeight::FLOAT, r_gender.maxWeight::FLOAT;
+					END LOOP;
+				CLOSE c_Gender;
+
+                OPEN c_Education(anioIn := current_year);
+					LOOP
+						FETCH c_Education INTO r_education_level;
+						EXIT WHEN NOT FOUND;
+						RAISE NOTICE '%   Education: - % - % - % - % - % - % - % - %', year, r_education_level.descripcion::TEXT, r_education_level.total::INT, r_education_level.avgAge::FLOAT, r_education_level.minAge::FLOAT, r_education_level.maxAge::FLOAT, r_education_level.avgWeight::FLOAT, r_education_level.minWeight::FLOAT, r_education_level.maxWeight::FLOAT;
+					END LOOP;
+				CLOSE c_Education;
+
+
+				-- Se ejecuta el cursor Anio
+				OPEN c_Anio(anioIn := current_year);
+					LOOP
+						FETCH c_Anio INTO r_anio;
+						EXIT WHEN NOT FOUND;
+						RAISE NOTICE '---------------------------- % - % - % - % - % - % - %', r_anio.total::INT, r_anio.avgAge::INT, r_anio.minAge::INT, r_anio.maxAge::INT, r_anio.avgWeight::INT, r_anio.minWeight::INT, r_anio.maxWeight;
+					END LOOP;
+				CLOSE c_Anio;
+
+				-- Se incrementa el anio seleccionado en una unidad
+				current_year = current_year + 1;
+
+			-- Salir del While
+			END LOOP;
+
+		-- Salir del IF
+		END IF;
 	END;
 $$ LANGUAGE plpgSQL;
+
+SELECT ReporteConsolidado(1);
+SELECT ReporteConsolidado(0);
+SELECT ReporteConsolidado(3);
+SELECT ReporteConsolidado(-1);
